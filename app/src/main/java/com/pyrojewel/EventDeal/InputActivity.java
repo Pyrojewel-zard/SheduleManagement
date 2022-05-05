b2376b064a47e65171fb10667629632f293e5648
Pyrojewel <2895687337@qq.com>
2022-05-05 12:50:30 +0800

    回退删除语音识别

--- app/src/main/java/com/pyrojewel/EventDeal/InputActivity.java
+++ app/src/main/java/com/pyrojewel/EventDeal/InputActivity.java
@@ -12,7 +12,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.pyrojewel.ui.input.InputGallery;
 import com.pyrojewel.ui.input.InputText;
-import com.pyrojewel.ui.input.InputVoice;
+import com.pyrojewel.ui.input.InputCamera;
 import com.example.myapplication.R;
 import com.google.android.material.tabs.TabLayout;
 
@@ -49,7 +49,7 @@ public class InputActivity extends AppCompatActivity {
                 case 1:
                     return new InputGallery();
                 case 2:
-                    return new InputVoice();
+                    return new InputCamera();
             }
             return null;
         }
